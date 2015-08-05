e94e57c101e993dfcecc476e955e31c46ad09997
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2015-08-05 13:49:42 +0200

    Action de stats

---
+++ classes/actions/Stats.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: manu
+ * Date: 05/08/15
+ * Time: 13:44
+ */
+
+namespace fr\manaur\buzzer;
+
+
+class Stats extends Action
+{
+    /**
+     * @return void
+     */
+    function action()
+    {
+        $this->sendFreeText(json_encode(array('nbSalons'=>Serveur::getInstance()->getSalons()->count(),'nbBuzzers'=>Serveur::getInstance()->getBuzzers()->count())));
+    }
+}
